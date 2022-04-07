17a008a94d2ccee52c95cd8ddb5ed33501aa6297
Meli Eliceo <melinae.06@gmail.com>
2022-04-07 15:01:12 -0300

    index

---
+++ index.js
@@ -0,0 +1,14 @@
+let curso = 'Git basico';
+let dia = 'Jueves';
+let horario = '10 a 13 hs';
+let plataforma = 'Zoom'; 
+
+console.log('curso = ' + curso);
+console.log('dia = ' + dia);
+console.log('horario = ' + horario);
+console.log('plataforma = ' + plataforma);
+
+document.writeln('curso = ' + curso+'<br>');
+document.writeln('dia = ' + dia +'<br>');
+document.writeln('horario = ' + horario +'<br>');
+document.writeln('plataforma = ' + plataforma +'<br>');
